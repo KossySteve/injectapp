296ccf5144aef124fae47f564acc2d06c103af83
KossySteve <kossyeze@gmail.com>
2022-04-06 12:14:08 -0700

    adjusted styling of about page

--- in-ject/src/Pages/AboutUs/AboutUs.js
+++ in-ject/src/Pages/AboutUs/AboutUs.js
@@ -1,22 +1,24 @@
 import React from "react";
 import image from "../../assets/images/aboutus.png";
-
+import "./AboutUs.scss";
 function AboutUs() {
   return (
     <section>
       <div className="dashboard">
-        <div className="dashboard__main">
-          <h2 className="dashboard__heading">About Us</h2>
-          <h3>
-            We are an online recruitment platform for health workers. We help you find and
-            post jobs, while building a successful health care support system
-            for professionals. Supporting our clients with prompt substitutes in
-            the workplace. We are expanding our reach and this requires nonstop
-            performance—a tough challenge in this industry but we are willing to
-            go the extra-mile.
-          </h3>
-          <div className="image-box">
-          <img src={image} alt="health professionals"/>
+        <div className="dashboard__main ">
+          <div className="image-container">
+            <div className="image-box">
+              <img className="image" src={image} alt="health professionals" />
+            </div>
+            <div className="text-box">
+              <h2 className="dashboard__heading">About Us</h2>
+              <h3>
+                We are an online recruitment platform for health workers. We
+                help you find and post jobs, while building a successful health
+                care support system for professionals. Supporting our clients
+                with prompt substitutes in the workplace.
+              </h3>
+            </div>
           </div>
         </div>
       </div>
