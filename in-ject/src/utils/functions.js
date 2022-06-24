a5380982ecb3063ee984f3eeda6235f1eaa2c185
KossySteve <kossyeze@gmail.com>
2022-06-23 17:36:21 -0700

    remove commented code

--- in-ject/src/utils/functions.js
+++ in-ject/src/utils/functions.js
@@ -6,10 +6,3 @@ export const apiUrlUsers = (end_point) => `${url}/users/${end_point}`;
 
 export const apiUrlJobs = (end_point) => `${url}/jobs/${end_point}`;
 
-// export const postReq = (apiCall, stateProp, displayfxn) => {
-//   axios.post(apiCall).then(({ data }) => {
-//     this.setState({ [stateProp]: data }, displayfxn);
-//   });
-// };
-
-//this.getVideo(apiUrl(""), "videos", this.fetchVideoDetails); apiUrlUsers getReq apiUrlJobs
