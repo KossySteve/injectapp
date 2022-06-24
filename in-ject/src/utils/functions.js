f3a856f647ca25287578089fc8194a16916a8dcc
KossySteve <kossyeze@gmail.com>
2022-06-23 17:33:13 -0700

    changed url

--- in-ject/src/utils/functions.js
+++ in-ject/src/utils/functions.js
@@ -1,11 +1,10 @@
+const url = "https://inject-app.herokuapp.com";
 export const formNotValid = (uploadData) =>
   Object.values(uploadData).includes("");
 
-export const apiUrlUsers = (end_point) =>
-  `http://localhost:5050/users/${end_point}`;
+export const apiUrlUsers = (end_point) => `${url}/users/${end_point}`;
 
-export const apiUrlJobs = (end_point) =>
-  `http://localhost:5050/jobs/${end_point}`;
+export const apiUrlJobs = (end_point) => `${url}/jobs/${end_point}`;
 
 // export const postReq = (apiCall, stateProp, displayfxn) => {
 //   axios.post(apiCall).then(({ data }) => {
